9a50632ab72f8ed2efe7a40bbaeb388f6ae20cd0
Edoardo henrique <2006edoardo@gmail.com>
2026-04-22 13:08:47 -0300

    feat: mapeamento completo das rotas de CRUD e login

--- vagas/urls.py
+++ vagas/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import HomeView, VagaListView
+from . import views
 
 urlpatterns = [
-    # Página de Boas-vindas
-    path('', HomeView.as_view(), name='home'),
+    # Públicas / Entrada
+    path('', views.HomeView.as_view(), name='home'),
+    path('vagas/', views.VagaListView.as_view(), name='vaga_list'),
     
-    # Página de Lista de Vagas 
-    path('vagas/', VagaListView.as_view(), name='vaga_list'),
+    # Login (Identificação)
+    path('login/candidato/', views.LoginCandidatoView.as_view(), name='login_candidato'),
+    path('login/empresa/', views.LoginEmpresaView.as_view(), name='login_empresa'),
+    
+    # CRUD Empresa (O motor do sistema)
+    path('vaga/nova/', views.VagaCreateView.as_view(), name='vaga_create'),
+    path('vaga/<int:pk>/editar/', views.VagaUpdateView.as_view(), name='vaga_update'),
+    path('vaga/<int:pk>/remover/', views.VagaDeleteView.as_view(), name='vaga_delete'),
 ]
